2cc931263b6e2ef05f1d612805bf53dd0eed0043
rayleeafar <rayleeafar@gmail.com>
2018-05-30 12:13:13 +0800

    add kill_by_name.sh

---
+++ kill_by_name.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#if [ $# -eq 0 ]
+if [ -z "$1" ]
+  then
+    echo "No process name supplied"
+    exit 1
+fi
+NAME=$1
+ps -aux | grep -v grep | grep -v $0 | grep $NAME
+read -p 'Are you sure to kill above process? ctrl+c abort else hit anykey to continue' careless
+
+kill -9 $(ps -aux | grep -v grep | grep -v $0 | grep $NAME | gawk '{print $2}')
